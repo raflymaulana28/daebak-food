647e9515f6d5004d0d2a8151085d1c522d3a27d0
geprekmendoan28 <73087923+geprekmendoan28@users.noreply.github.com>
2021-10-04 22:42:00 +0700

    :rocket: update get hello function

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Halo Welcome Daebak Food, Please Login To Access API!';
   }
 }
